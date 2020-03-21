36e3f3a25ebd51155bf339f23a0a6a59370b48ce
satasoy6 <atasoyseyma@yahoo.com>
2020-03-20 22:51:11 -0400

    Repl questions added for reference

---
+++ src/ReplHomeworks/Repl94.java
@@ -0,0 +1,38 @@
+package ReplHomeworks;
+
+import java.util.Scanner;
+
+public class Repl94 {
+	public static void main(String[] args) {
+		
+	
+//		For you to do:
+		
+//				If browser is ChRoME it should print the: 
+//				"Proceed with Chrome browser"
+		
+//				If browser is FireFOX it should print the: 
+//				"Proceed with Firefox browser"
+		
+//				If browser is IE it should print the:
+//				"Proceed with IE browser"
+		
+//				If any other browser it should print the:  
+//				"Invalid browser"
+			
+			Scanner sc = new Scanner(System.in); 
+		    
+
+			System.out.println("Enter the browser name to proceed further with execution");
+			
+			String browser = sc.nextLine();
+			
+			
+			
+	}
+
+
+	}
+
+	
+}
